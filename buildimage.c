c07df837880701bd3e044c8aa6efeaaa1a881bf9
Guilherme Gonzaga <guilhermgonzaga@gmail.com>
2020-03-15 17:45:44 -0400

    Use parameters in argv, directory cleanup

--- buildimage.c
+++ buildimage.c
@@ -1,7 +1,7 @@
 /* Author(s): Felipe Hiroshi Baron e Guilherme Gonzaga
  * Creates operating system image suitable for placement on a boot disk
 */
-/* TODO: Comment on the status of your submission. Largely unimplemented */
+/* Implementation complete. */
 #include <assert.h>
 #include <elf.h>
 #include <errno.h>
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 
 #define IMAGE_FILE "./image"
@@ -18,10 +19,10 @@
 #define BOOTLOADER_SIG_OFFSET 0x1fe /* offset for boot loader signature */
 #define BOOTLOADER_ADRESS 0X7c00    /* boot loader address in memory */
 #define ELF32_HEADER_SIZE 52        /* Elf32 header size in bytes */
-// more defines...
+
 
 /* Reads in an executable file in ELF format*/
-Elf32_Phdr * read_exec_file(FILE **execfile, char *filename, Elf32_Ehdr **ehdr){
+Elf32_Phdr *read_exec_file(FILE **execfile, const char *filename, Elf32_Ehdr **ehdr){
   Elf32_Phdr *phdr = malloc(sizeof(Elf32_Phdr));
   size_t items_rw;
 
@@ -36,9 +37,10 @@ Elf32_Phdr * read_exec_file(FILE **execfile, char *filename, Elf32_Ehdr **ehdr){
   return phdr;
 }
 
+
 /* Writes the bootblock to the image file */
 void write_bootblock(FILE **imagefile, FILE *bootfile, Elf32_Ehdr *boot_header, Elf32_Phdr *boot_phdr){
-  unsigned char *buffer = malloc(boot_phdr->p_filesz);
+  uint8_t *buffer = malloc(boot_phdr->p_filesz);
   const size_t padding_length = SECTOR_SIZE - (boot_phdr->p_filesz % SECTOR_SIZE);
   size_t items_rw;
 
@@ -50,7 +52,7 @@ void write_bootblock(FILE **imagefile, FILE *bootfile, Elf32_Ehdr *boot_header, 
   items_rw = fwrite(buffer, boot_phdr->p_filesz, 1, *imagefile);
   assert(items_rw == 1);
 
-  unsigned char *buffer_padding = calloc(padding_length, 1);
+  uint8_t *buffer_padding = calloc(padding_length, 1);
   items_rw = fwrite(buffer_padding, 1, padding_length, *imagefile);
   assert(items_rw == padding_length);
 
@@ -65,12 +67,12 @@ void write_bootblock(FILE **imagefile, FILE *bootfile, Elf32_Ehdr *boot_header, 
   free(buffer_padding);
 }
 
+
 /* Writes the kernel to the image file */
 void write_kernel(FILE **imagefile, FILE *kernelfile, Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr){
-  unsigned char *buffer = malloc(kernel_phdr->p_filesz);
+  uint8_t *buffer = malloc(kernel_phdr->p_filesz);
   const size_t padding_length = SECTOR_SIZE - (kernel_phdr->p_filesz % SECTOR_SIZE);
   size_t items_rw;
-  const char zero = 0;
 
   fseek(kernelfile, kernel_phdr->p_offset, SEEK_SET);
   items_rw = fread(buffer, kernel_phdr->p_filesz, 1, kernelfile);
@@ -80,34 +82,34 @@ void write_kernel(FILE **imagefile, FILE *kernelfile, Elf32_Ehdr *kernel_header,
   items_rw = fwrite(buffer, kernel_phdr->p_filesz, 1, *imagefile);
   assert(items_rw == 1);
 
-  unsigned char *buffer_padding = calloc(padding_length, 1);
+  uint8_t *buffer_padding = calloc(padding_length, 1);
   items_rw = fwrite(buffer_padding, 1, padding_length, *imagefile);
   assert(items_rw == padding_length);
  
-
   free(buffer);
   free(buffer_padding);
 }
 
+
 /* Counts the number of sectors in the kernel */
-int count_kernel_sectors(Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr){
-  float sectors = (float)kernel_phdr->p_filesz/SECTOR_SIZE;
-  if(kernel_phdr->p_filesz%SECTOR_SIZE != 0){
-    sectors = (int)sectors+1;
+int32_t count_kernel_sectors(Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr){
+  float sectors = (float)kernel_phdr->p_filesz / SECTOR_SIZE;
+  if(kernel_phdr->p_filesz % SECTOR_SIZE != 0){
+    sectors = (int32_t)sectors + 1;
   }
   return sectors;
 }
 
-/* Records the number of sectors in the kernel */
-void record_kernel_sectors(FILE **imagefile, Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr, int num_sec){
-  fseek(*imagefile, 2, SEEK_SET);
-  fwrite(&num_sec, sizeof(int), 1, *imagefile);
 
+/* Records the number of sectors in the kernel */
+void record_kernel_sectors(FILE **imagefile, Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr, int32_t num_sec){
+  fseek(*imagefile, 2, SEEK_SET);
+  fwrite(&num_sec, sizeof(int32_t), 1, *imagefile);
 }
 
 
 /* Prints segment information for --extended option */
-void extended_opt(Elf32_Phdr *bph, int k_phnum, Elf32_Phdr *kph, int num_sec){
+void extended_opt(Elf32_Phdr *bph, int k_phnum, Elf32_Phdr *kph, int32_t num_sec){
   int padding_up_boot = (bph->p_filesz/SECTOR_SIZE)+ SECTOR_SIZE;
   int padding_up_kernel;
 
@@ -129,16 +131,23 @@ void extended_opt(Elf32_Phdr *bph, int k_phnum, Elf32_Phdr *kph, int num_sec){
   printf("\t\toffset 0x%04x\t\tvaddr 0x%04x\n", kph->p_offset, kph->p_vaddr);
   printf("\t\tfilesz 0x%04x\t\tmemsz 0x%04x\n", kph->p_filesz, kph->p_memsz);
   printf("\t\twriting 0x%04x bytes\n", kph->p_memsz);
-   printf("\t\tpadding up to 0x%04x bytes\n", padding_up_kernel);
+  printf("\t\tpadding up to 0x%04x bytes\n", padding_up_kernel);
+
   /* print kernel size in sectors */
   printf("os_size: %d sectors\n", num_sec);
-
 }
-// more helper functions...
+
 
 /* MAIN */
-// ignore the --vm argument when implementing (project 1)
 int main(int argc, char **argv){
+  if (argc < 3 || argc > 5) {
+    printf("Usage: %s %s", argv[0], ARGS);
+    return EXIT_FAILURE;
+  }
+
+  const char *bootblock_filename = argv[argc-2];
+  const char *kernel_filename = argv[argc-1];
+
   FILE *kernelfile, *bootfile,*imagefile;  //file pointers for bootblock,kernel and image
   Elf32_Ehdr *boot_header = malloc(sizeof(Elf32_Ehdr));//bootblock ELF header
   Elf32_Ehdr *kernel_header = malloc(sizeof(Elf32_Ehdr));//kernel ELF header
@@ -146,37 +155,40 @@ int main(int argc, char **argv){
   Elf32_Phdr *boot_program_header; //bootblock ELF program header
   Elf32_Phdr *kernel_program_header; //kernel ELF program header
 
-  int num_sec;
+  int32_t num_sec;
 
   /* build image file */
   imagefile = fopen(IMAGE_FILE, "wb");
   assert(imagefile != NULL);
 
   /* read executable bootblock file */
-  bootfile = fopen("bootblock", "rb");
+  bootfile = fopen(bootblock_filename, "rb");
   assert(bootfile != NULL);
-  boot_program_header = read_exec_file(&bootfile, "bootblock", &boot_header);
-  /* write bootblock */
+  boot_program_header = read_exec_file(&bootfile, bootblock_filename, &boot_header);
+
+  /* write bootblock to image*/
   write_bootblock(&imagefile, bootfile, boot_header, boot_program_header);
 
   /* read executable kernel file */
-  kernelfile = fopen("kernel", "rb");
+  kernelfile = fopen(kernel_filename, "rb");
   assert(kernelfile != NULL);
-  kernel_program_header = read_exec_file(&kernelfile, "kernel", &kernel_header);
+  kernel_program_header = read_exec_file(&kernelfile, kernel_filename, &kernel_header);
 
   /* write kernel segments to image */
   write_kernel(&imagefile, kernelfile, kernel_header, kernel_program_header);
 
   /* tell the bootloader how many sectors to read to load the kernel */
   num_sec = count_kernel_sectors(kernel_header, kernel_program_header);
-
-
-  record_kernel_sectors(&imagefile,kernel_header, kernel_program_header,num_sec);
+  record_kernel_sectors(&imagefile, kernel_header, kernel_program_header, num_sec);
 
   /* check for  --extended option */
   if(!strncmp(argv[1],"--extended",11)){
     extended_opt(boot_program_header, kernel_header->e_phnum, kernel_program_header, num_sec);
   }
 
-  return 0;
+  fclose(imagefile);
+  fclose(bootfile);
+  fclose(kernelfile);
+
+  return EXIT_SUCCESS;
 } // ends main()
