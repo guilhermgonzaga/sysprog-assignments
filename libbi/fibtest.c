77d0797ef81b6add4472f5029efa93ca4fb15ae5
Guilherme Gonzaga de Andrade <guilhermgonzaga@gmail.com>
2020-04-28 22:51:26 -0400

    Improve fibtest

--- libbi/fibtest.c
+++ libbi/fibtest.c
@@ -2,17 +2,12 @@
 #include <time.h>
 #include <BigInt.h>
 
-#define FIB_LIMIT 5500
-// #define OPTIMIZED
-
-#ifdef OPTIMIZED
-	#include <omp.h>
-#endif
+#define FIB_LIMIT 5500  // Biggest number in the sequence to be calculated
 
 
 void fib(int n) {
 	BigInt a = {0};
-	BigInt b = {0, [BIGINT_LEN-1] = 1};
+	BigInt b = {[BIGINT_LEN-1] = 1};
 	BigInt c;
 
 	for (int i = 1; i < n; i++) {
@@ -24,7 +19,7 @@ void fib(int n) {
 
 int main(int argc, const char *argv[]) {
 	if (argv[1] == NULL) {
-		fprintf(stderr, "Usage: fibtest <output_file.csv>\n");
+		printf("Usage: fibtest <output_file.csv>\n");
 		return 1;
 	}
 
@@ -34,19 +29,15 @@ int main(int argc, const char *argv[]) {
 		return 1;
 	}
 
-	fprintf(output, "sep=,\nNumber,CPU Time (s)\n");
+	fprintf(output, "sep=,\nNumber,CPU Time (ms)\n");
 
-#ifdef OPTIMIZED
-	#pragma omp parallel for schedule(dynamic)
-#endif
 	for (int i = 500; i <= FIB_LIMIT; i += 500) {
 		clock_t start = clock();
-		fib(i);
+		for (int j = 0; j < 1000; ++j) {
+			fib(i);
+		}
 		clock_t end = clock();
 
-#ifdef OPTIMIZED
-	#pragma omp critical
-#endif
 		fprintf(output, "%d,%G\n", i, (double)(end - start) / CLOCKS_PER_SEC);
 	}
 
