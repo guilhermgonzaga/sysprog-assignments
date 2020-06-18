f5e539cd78e9645516b5e03287f4e12144e8957b
Guilherme Gonzaga de Andrade <guilhermgonzaga@gmail.com>
2020-06-18 14:13:36 -0400

    Add simple test loop

--- shell/src/main.c
+++ shell/src/main.c
@@ -4,9 +4,23 @@
  */
 
 #include <common.h>
+#include <io.h>
+#include <lexing.h>
 
 
-int main(int argc, const char *argv[], const char *envp[])
-{
+int main(int argc, const char *argv[], const char *envp[]) {
+	set_input_stream(stdin);
+	set_output_stream(stdout);
+
+	while (read_cmdline() == ERR_NO_ERROR) {
+		token_t tok;
+		tokenize_input(cmdline_buffer);
+
+		while (get_next_token(&tok) == ERR_NO_ERROR) {
+			fprintf(stderr, "%.*s  :::  ", (int)tok.length, tok.begin);
+		}
+		fprintf(stderr, "\n");
+	}
+
 	return 0;
 }
